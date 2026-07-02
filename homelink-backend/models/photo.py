55dac2714713169775fcc628a8fbd20a232ebe13
Daoudesp <daoudacisse@esp.sn>
2026-07-02 16:02:48 +0000

    Suppression date_expiration

--- homelink-backend/models/photo.py
+++ homelink-backend/models/photo.py
@@ -6,6 +6,5 @@ class Photo(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     annonce_id = db.Column(db.Integer, db.ForeignKey('annonces.id'), nullable=False)
     url = db.Column(db.String(500), nullable=False)
-    ordre = db.Column(db.Integer, default=0)
 
     annonce = db.relationship('Annonce', backref='photos')
